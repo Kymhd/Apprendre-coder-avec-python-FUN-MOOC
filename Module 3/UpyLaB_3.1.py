1ed80990be49703fb1dce9e6be788f4750efb84a
Kayoum <86859599+Kymhd@users.noreply.github.com>
2021-11-13 16:36:21 +0000

    Create UpyLaB_3.1.py

---
+++ Module 3/UpyLaB_3.1.py
@@ -0,0 +1,16 @@
+"""
+Écrire un programme qui lit 3 nombres entiers, et qui, si au moins deux d’entre eux ont la même valeur,
+imprime cette valeur (le programme n’imprime rien dans le cas contraire).
+"""
+
+n = int(input())
+m = int(input())
+s = int(input())
+if n == s or n == m:
+    print(n)
+elif n == m or m == s:
+     print(m)
+    
+#les entrées   
+42 42 42 #42
+1 2 3 #le programme n'affiche rien
