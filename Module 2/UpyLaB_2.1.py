ef947bd20dcdc70330f03d319bc549245abb822b
Kayoum <86859599+Kymhd@users.noreply.github.com>
2021-11-13 16:31:12 +0000

    Create UpyLaB_2.1.py

---
+++ Module 2/UpyLaB_2.1.py
@@ -0,0 +1,22 @@
+"""
+Écrire un programme qui assigne :
+
+la valeur entière 36 à la variable x ;
+
+la valeur entière résultat de 36 fois 36 à la variable produit ;
+
+la valeur entière résultat de la division entière de 36 par 5 à la variable div_entiere ;
+
+la valeur entière résultat de 15 exposant 15 à la variable expo ;
+
+la valeur float 3.14159 à la variable pi ;
+
+la valeur chaîne de caractères "Bonjour" à la variable mon_texte.
+"""
+
+x = 36
+produit = 1296
+div_entiere = 7
+expo = 437893890380859375
+pi = 3.14159
+mon_texte = "Bonjour"
