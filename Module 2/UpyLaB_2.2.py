0ec4aff8f804b32d1020f3d2b1f05256203a1c7a
Kayoum <86859599+Kymhd@users.noreply.github.com>
2022-02-21 14:35:32 +0000

    Create UpyLaB_2.2.py

---
+++ Module 2/UpyLaB_2.2.py
@@ -0,0 +1,17 @@
+"""
+Écrire un programme qui imprime (donc grâce à la fonction print) 
+la moyenne arithmétique de deux nombres de type float lus en entrée 
+(c'est-à-dire grâce à des appels à la fonction input) .
+
+On rappelle que la moyenne arithmétique de deux nombres a et b est égale à  \frac{a+b}{2}.
+"""
+
+a = float(input())
+b = float(input())
+y = (a + b)/2
+print(y)         
+
+#a = 2.0
+#b = 3.0
+#le résultat à imprimer vaudra :
+#2.5
